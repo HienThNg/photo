ef3626d69665542986669d9cbfe434d84b1dee2e
HienThNg <108725055+HienThNg@users.noreply.github.com>
2024-11-17 01:46:17 +0700

    Update streamlit_images.py

--- streamlit_images.py
+++ streamlit_images.py
@@ -20,37 +20,6 @@ image_urls = [
 
 # st.write('################################################################################')
 
-columns = """
-<style>
-    div.container {
-        display: flex;
-        flex-flow: row wrap;
-        gap: 2em;
-        align-items: flex-start;
-        align-content: flex-start;
-        justify-content: flex-start;
-    }
-    div.item {
-        flex: 1 1 auto;
-    }
-</style>
-<div class="container">
-    <div class="item">
-        <table>
-        <tr><th>alpha beta</th><th>gamma delta</th><th>epsilon zeta</th></tr>
-        <tr><td>123</td><td>456</td><td>789</td></tr>
-        </table>
-    </div>
-    <div class="item">
-        <table>
-        <tr><th>eta theta</th><th>iota kappa</th><th>lambda mu</th></tr>
-        <tr><td>123</td><td>456</td><td>789</td></tr>
-        </table>
-    </div>
-</div>
-"""
-
-st.markdown(columns, unsafe_allow_html=True)
 
 def fix_mobile_columns():    
     st.write('''<style>
@@ -96,17 +65,17 @@ st.image(current_image, use_column_width=True)
 #         navigate_image("next")
 
 # Create columns layout: Left (for "Back"), Center (for image), Right (for "Next")
-col6, col7= st.columns([1,1])  # Adjust column ratios for spacing
+col1, col2= st.columns([1,1])  # Adjust column ratios for spacing
 
 # Left column: Back button
-with col6:
+with col1:
     st.write("")  # Spacer for vertical alignment
     if st.button("Back"):
         navigate_image("back")
 
 
 # Right column: Next button
-with col7:
+with col2:
     st.write("")  # Spacer for vertical alignment
     if st.button("Next"):
         navigate_image("next")
