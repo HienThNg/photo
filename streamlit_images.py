7d0f1bdeed41367bc0153ddb37152e9461ae47de
HienThNg <108725055+HienThNg@users.noreply.github.com>
2024-11-17 01:33:46 +0700

    Update streamlit_images.py

--- streamlit_images.py
+++ streamlit_images.py
@@ -30,6 +30,15 @@ st.write('''<style>
 .css-1l269bu {max-width:20% !important;}
 </style>''', unsafe_allow_html=True)
 
+def fix_mobile_columns():    
+    st.write('''<style>
+    [data-testid="column"] {
+        width: calc(16.6666% - 1rem) !important;
+        flex: 1 1 calc(16.6666% - 1rem) !important;
+        min-width: calc(16.6666% - 1rem) !important;
+    }
+    </style>''', unsafe_allow_html=True)
+
 # Initialize session state for the current image index
 if "current_image" not in st.session_state:
     st.session_state.current_image = 0
