6ab0cee84a610052c2a5493177cfdbfdf113621d
HienThNg <108725055+HienThNg@users.noreply.github.com>
2024-11-17 21:59:37 +0700

    Update streamlit_images.py

--- streamlit_images.py
+++ streamlit_images.py
@@ -40,50 +40,20 @@ def navigate_image(direction):
 current_image = image_urls[st.session_state.current_image]
 st.image(current_image, use_column_width=True)
 
-# # Create columns layout: Left (for "Back"), Center (for image), Right (for "Next")
-# col1, col2, col3, col4 = st.columns([3,2,2,3])  # Adjust column ratios for spacing
-
-# # Left column: Back button
-# with col2:
-#     st.write("")  # Spacer for vertical alignment
-#     if st.button("Back"):
-#         navigate_image("back")
-
-
-# # Right column: Next button
-# with col3:
-#     st.write("")  # Spacer for vertical alignment
-#     if st.button("Next"):
-#         navigate_image("next")
-
 # Create columns layout: Left (for "Back"), Center (for image), Right (for "Next")
-col1, col2= st.columns([1,1])  # Adjust column ratios for spacing
-
-
-st.write('''<style>
-[data-testid="column"] {
-    width: calc(25% - 1rem) !important;
-    flex: 1 1 calc(25% - 1rem) !important;
-    min-width: calc(20% - 1rem) !important;
-}
-.css-1l269bu {max-width:20% !important;}
-</style>''', unsafe_allow_html=True)
+col1, col2, col3, col4 = st.columns([3,2,2,3])  # Adjust column ratios for spacing
 
 # Left column: Back button
-with col1:
-    st.write("")  # Spacer for vertical alignment
-    if st.button("Back"):
+with col2:
+    if st.button("Ảnh trước"):
         navigate_image("back")
 
 
 # Right column: Next button
-with col2:
-    st.write("")  # Spacer for vertical alignment
-    if st.button("Next"):
+with col3:
+    if st.button("Ảnh tiếp theo"):
         navigate_image("next")
 
 
 
-with col8:
-    st.button("Button 4")
 
