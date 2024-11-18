14d9bb9b6fb0177bac2a781f774e0b840b70688c
HienThNg <108725055+HienThNg@users.noreply.github.com>
2024-11-18 15:51:38 +0700

    Update thuyhien_trandung.py

--- thuyhien_trandung.py
+++ thuyhien_trandung.py
@@ -91,7 +91,7 @@ st.image('./Online/map-bao-hy.png')
 
 #-----------------------------------------------------------------------------------------------
 # Lời cảm ơn
-st.write('''<style>
+st.markdown('''<style>
 Kính gửi Quý Khách, chúng tôi xin chân thành cảm ơn Quý Khách đã dành thời gian đến tham dự đám cưới của chúng tôi. Sự có mặt của Quý Khách đã làm cho ngày trọng đại của chúng tôi thêm ý nghĩa và vui vẻ. Chúng tôi xin gửi lời chúc sức khỏe và hạnh phúc đến Quý Khách và Gia đình.
 {text-align: center}</style>''')
 
